3ea696a2074ff5740798e8b4776809bc55f526e1
adigunatp <adiguna.triputra@gmail.com>
2018-07-26 11:57:58 +0700

    fix bug disable next month button pmo

--- src/main/java/com/pusilkom/hris/controller/IndexController.java
+++ src/main/java/com/pusilkom/hris/controller/IndexController.java
@@ -218,6 +218,12 @@ public class IndexController
             roleMap.put(role.getId(), role.getNamaRole());
         }
 
+        LocalDate next = periodeDate.plusMonths(1);
+
+        if(periodeNow.plusMonths(1).isAfter(next)){
+            model.addAttribute("next", periodeDate.plusMonths(1));
+        }
+
         model.addAttribute("rekapRoleMap", rekapRoleMap);
         model.addAttribute("roles", roleMap);
         model.addAttribute("date_today", dateToday);
@@ -228,10 +234,10 @@ public class IndexController
         model.addAttribute("totalPercentage", totalPerc);
         model.addAttribute("totalGreen", (int) (255 - (2.55*totalPerc)));
         model.addAttribute("totalRed", (int) (2.55*totalPerc));
-        model.addAttribute("next", periodeDate.plusMonths(1));
         model.addAttribute("current", periodeDate);
         model.addAttribute("previous", periodeDate.minusMonths(1));
         model.addAttribute("periodeNow", periodeNow);
+        model.addAttribute("invalidMonth", next);
         return "index-pmo";
     }
 
